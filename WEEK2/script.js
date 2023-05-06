555affc71870cdf742c4831fdb05744a53bc2e21
Your Nameexit <bertrandmelanie028@gmail.com>
2023-05-06 10:58:43 +0400

    update

---
+++ WEEK2/script.js
@@ -0,0 +1,10 @@
+const fruits= ["Banana", "Apples","Orange","Blueberries"]
+
+fruits.sort(function(a, b){return a-b});
+fruits.push("kiwi")
+fruits.splice(0,1);
+fruits.shift(0)
+console.log(fruits);
+fruitsName=fruits.(sort);
+console.log(fruitsName);
+
