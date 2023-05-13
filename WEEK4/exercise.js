2a8e25fb547bc5bb272bd8d6dadb095111f3ffa4
Your Nameexit <bertrandmelanie028@gmail.com>
2023-05-13 09:39:34 +0400

    Updte

---
+++ WEEK4/exercise.js
@@ -0,0 +1,19 @@
+function calculateTip () {
+    const bill= float(prompt("What is the amount of the bill"));
+}
+ let= tip (50)
+ if (bill < 50) {
+    tip = 0.2;
+ }
+ else if (bill >=50 && bill <= 200) {
+    tip= .015;
+
+ }
+  else (bill >200){
+    tip= 0.1;
+  }
+
+  const tipAmount = bill *  tip;
+  const totalBill = bill + tipAmount
+
+  console.log('Tip amount: $${')
