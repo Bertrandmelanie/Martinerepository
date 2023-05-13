a84f45392c99919b82c169f5c7db5d18962ca13a
Your Nameexit <bertrandmelanie028@gmail.com>
2023-05-13 09:39:58 +0400

    update

---
+++ WEEK4/Day1.js
@@ -0,0 +1,12 @@
+function infoAboutMe() {
+const name = "Martine";
+const age = 24;
+const hobbies = "singing, playing voleyball";
+console.log('my name is ${name}, I am ${age} and my hobbies are ${hobbies}');
+}
+
+function infoAboutPerson(){
+    const personName = "David"
+    const personAge = 42;
+    const personFavoriteColor = blue
+console.log ('your name is ${personName}, you are ${personAge}years old, and your favorite color is ${personFavoriteColor}');}
