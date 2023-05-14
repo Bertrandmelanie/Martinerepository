cbb168b40dc02c4bd4a34f4247b0b4095ad9186e
Your Nameexit <bertrandmelanie028@gmail.com>
2023-05-14 22:34:19 +0400

    updated

---
+++ WEEK3/Day 4/2ndexercise.js
@@ -0,0 +1,11 @@
+var newDog = "Chihuahua"
+console.log("numbers of letter in newDog:", newDog.length);
+
+console.log("newDog in uppercase:", newDog.toUpperCase());
+console.log("newDog in lowercase:", newDog.toLowerCase());
+
+if (newDog === "Chihuahua") {
+    console.log("I love Chihuahuas, it’s my favorite dog breed");
+  } else {
+    console.log("I dont care, I prefer cats");
+  }
