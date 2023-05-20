f0ac98eb763af18089dcdefe464ed7e55d1e813e
Your Nameexit <bertrandmelanie028@gmail.com>
2023-05-20 09:29:17 +0400

    updated

---
+++ WEEK4/domexercise.js
@@ -0,0 +1,7 @@
+let p = document.getElementById("text")
+p.style.fontSize = "20px";
+p.style.fontFamily = "New Time Roman";
+p.style.color = "red";
+p.style.fontWeight = "bold";
+
+p.style.cssText = "font-size: 20px; font.family: New Time Roman; p.style.color:red;"
