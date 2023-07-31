9412f918fe7c98e7b8abf804288654f46f713139
melanie <melanie@test.com>
2023-07-31 15:29:57 +0400

    update

--- WEEK4/domexercise.js
+++
@@ -1,7 +0,0 @@
-let p = document.getElementById("text")
-p.style.fontSize = "20px";
-p.style.fontFamily = "New Time Roman";
-p.style.color = "red";
-p.style.fontWeight = "bold";
-
-p.style.cssText = "font-size: 20px; font.family: New Time Roman; p.style.color:red;"
