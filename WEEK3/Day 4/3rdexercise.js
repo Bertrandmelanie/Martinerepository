095bde88c3d331bc5ba7ddabadc63863b9a316c0
melanie <melanie@test.com>
2023-07-31 14:44:16 +0400

    update

--- WEEK3/Day 4/3rdexercise.js
+++
@@ -1,8 +0,0 @@
-let userInput = prompt("Please enter a number:");
-let number = parseInt(userInput);
-
-if (number % 1 === 0) {
-  console.log(`${number} is an even number`);
-} else {
-  console.log(`${number} is an odd number`);
-}
